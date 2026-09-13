9c151bb3c461a6e01bffa22f2e1cf7a2f0950122
Plynexa <plynexa@gmail.com>
2026-09-12 23:25:58 -0300

    Restringe conversao IPv4 sem alterar std::bitset

--- ferramentas/aplicar_compatibilidade_boost.sh
+++ ferramentas/aplicar_compatibilidade_boost.sh
@@ -10,7 +10,8 @@ sed -i 's/m_service\.dispatch(boost::bind(&Connection::onStop, this));/boost::as
 sed -i 's/boost::posix_time::seconds(Connection::readTimeout)/boost::posix_time::seconds(static_cast<int64_t>(Connection::readTimeout))/g' "$arquivo"
 sed -i 's/boost::posix_time::seconds(Connection::writeTimeout)/boost::posix_time::seconds(static_cast<int64_t>(Connection::writeTimeout))/g' "$arquivo"
 
-# address_v4::to_ulong foi removido nas versoes atuais; to_uint preserva a semantica desejada.
-find servidor/src -type f \( -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 sed -i 's/\.to_ulong()/\.to_uint()/g'
+# address_v4::to_ulong foi removido nas versoes atuais. Restrinja a troca ao endereco IPv4
+# para nao tocar em std::bitset::to_ulong(), que continua correto.
+find servidor/src -type f \( -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 sed -i 's/\.to_v4()\.to_ulong()/\.to_v4()\.to_uint()/g'
 
 echo 'Camada de compatibilidade Boost aplicada.'
